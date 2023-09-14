4fd25b61c41be4528893f504e5ebdd186e1a3c59
thansen <thansendhrit.lahare161@gmail.com>
2023-09-14 16:33:05 +0530

    14/09/2023

--- src/SecondLargestSecondSmallest.java
+++ src/SecondLargestSecondSmallest.java
@@ -7,8 +7,10 @@ https://www.codingninjas.com/studio/problems/ninja-and-the-second-order-elements
 
 public class SecondLargestSecondSmallest {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(getSecondOrderElements(array.length, array)));
+        int[] array = {1,43,-167,54,3839,3839,34,54,54};
+        //System.out.println(Arrays.toString(getSecondOrderElements(array.length, array)));
+        //System.out.println(secondlargest(array));
+        System.out.println(secondlargestoptimal(array));
     }
     public static int[] getSecondOrderElements(int n, int[] arr) {
         int firstLargest = Integer.MIN_VALUE;
@@ -34,5 +36,38 @@ public class SecondLargestSecondSmallest {
 
         }
         return new int[] { secondLargest, secondSmallest};
+    }
+    // bf solution for finding the secondlargest
+    // sorting nlong.
+    // worstcase big oh of n. if dublicate element.
+    // nlong + n
+
+    public static int secondlargest(int[] array){
+        Arrays.sort(array);
+        int largest = array[array.length-1];
+        for (int i = array.length-1; i >=0 ; i--) {
+            if (array[i] != largest){
+                return array[i];
+            }
+        }
+        return -1;
+    }
+    //secondlargest findin optimat solution
+    // time complexity of the approach is Bigoh of N.
+    // where N is the length of the array.
+    // assuming all the comparision will take constatnt time.
+    public static int secondlargestoptimal(int[] array){
+        int secondlargest = Integer.MIN_VALUE;
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] > largest){
+                secondlargest = largest;
+                largest = array[i];
+            }
+            if (array[i]<largest && array[i] > secondlargest){
+                secondlargest = array[i];
+            }
+        }
+        return secondlargest;
     }
 }
---
+++ src/checkArrayIsSortedInNondecendingorder.java
@@ -0,0 +1,13 @@
+public class checkArrayIsSortedInNondecendingorder {
+    public static void main(String[] args) {
+
+    }
+    static boolean isSorted(int[] array){
+        boolean flag = true;
+        int i =0;
+        while (array[i] <= array[i+1] && i < array.length){
+        }
+        flag = false;
+        return flag;
+    }
+}
