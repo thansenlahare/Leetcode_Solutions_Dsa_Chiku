7d9bf7dd2662904a2f2f0900988f5b5c225357e4
thansen <thansendhrit.lahare161@gmail.com>
2023-09-15 01:18:00 +0530

    15092023

---
+++ src/RotatethearraybyKsteps189.java
@@ -0,0 +1,14 @@
+public class RotatethearraybyKsteps189 {
+    public static void main(String[] args) {
+
+    }
+
+    public void rotate(int[] nums, int k) {
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[(i + k) % nums.length] = nums[i];
+        }
+        System.arraycopy(temp, 0, nums, 0, nums.length);
+    }
+
+}
--- src/checkArrayIsSortedInNondecendingorder.java
+++ src/checkArrayIsSortedInNondecendingorder.java
@@ -1,13 +1,21 @@
 public class checkArrayIsSortedInNondecendingorder {
     public static void main(String[] args) {
+        int[] arr = {1,2,2,3,4,5,89};
+        System.out.println(isSorted(arr));
 
     }
     static boolean isSorted(int[] array){
         boolean flag = true;
         int i =0;
-        while (array[i] <= array[i+1] && i < array.length){
+        while(i < array.length-1){
+            if (array[i]<=array[i+1]){
+                flag = true;
+            }
+            else{
+                return false;
+            }
+            i++;
         }
-        flag = false;
         return flag;
     }
 }
